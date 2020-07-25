78962ac2daa1c6f36485db455207913165e1ae06
mostafa <mostafa.mm661@gmail.com>
2020-07-25 19:06:13 +0200

    first commit

---
+++ ppmtooltefa/src/main/java/com/mostafa/spring/PpmtooltefaApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtooltefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtooltefaApplication.class, args);
+	}
+
+}
---
+++ ppmtooltefa/src/test/java/com/mostafa/spring/PpmtooltefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtooltefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
